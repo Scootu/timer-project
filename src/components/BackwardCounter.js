bec7c35124b7a820949df404c606e37c47a0437b
aneshamdaoui <bacetd2004@gmail.com>
2023-07-26 10:44:04 +0100

    f s45

---
+++ src/components/BackwardCounter.js
@@ -0,0 +1,9 @@
+import useCounter from "../hook/use-counter";
+import Card from "./Card";
+
+const BackwardCounter = () => {
+  const counter = useCounter(false);
+  return <Card>{counter}</Card>;
+};
+
+export default BackwardCounter;
